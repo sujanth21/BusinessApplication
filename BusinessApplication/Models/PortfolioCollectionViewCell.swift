a1d565894f47e2f74d7eea7608c0cbcedc977d95
Sujanth <sujanth1987@gmail.com>
2017-09-15 08:25:29 +1000

    Portfolio added

---
+++ BusinessApplication/Models/PortfolioCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PortfolioCollectionViewCell.swift
+//  BusinessApplication
+//
+//  Created by Sujanth Sebamalaithasan on 15/9/17.
+//  Copyright © 2017 Sujanth Sebamalaithasan. All rights reserved.
+//
+
+import UIKit
+
+class PortfolioCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellImage: UIImageView!
+}
