03f0c93b7fd277b3f41741f550e7e4e38ee63ada
Sujanth <sujanth1987@gmail.com>
2017-09-17 12:01:19 +1000

    Added pin location in map

--- BusinessApplication/Controllers/ContactUs/ContactUsViewController.swift
+++ BusinessApplication/Controllers/ContactUs/ContactUsViewController.swift
@@ -13,11 +13,25 @@ class ContactUsViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    let latitude = -37.755479
+    let longitude = 144.996658
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let span = MKCoordinateSpanMake(0.005, 0.005)
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: latitude, longitude: longitude), span: span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        let pinLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        let pinAnnotation = MKPointAnnotation()
+        pinAnnotation.coordinate = pinLocation
+        pinAnnotation.title = "My Company"
+        pinAnnotation.subtitle = "45 Hutton Street, Thornbury, Australia."
+        self.mapView.addAnnotation(pinAnnotation)
     }
 
     override func didReceiveMemoryWarning() {
