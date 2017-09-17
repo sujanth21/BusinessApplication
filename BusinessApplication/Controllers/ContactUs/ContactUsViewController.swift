2812a3b2c1f6e0000ec0a2e64bfd7de9478af64e
Sujanth <sujanth1987@gmail.com>
2017-09-17 12:08:09 +1000

    directions function added

--- BusinessApplication/Controllers/ContactUs/ContactUsViewController.swift
+++ BusinessApplication/Controllers/ContactUs/ContactUsViewController.swift
@@ -34,20 +34,13 @@ class ContactUsViewController: UIViewController {
         self.mapView.addAnnotation(pinAnnotation)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func directionBtnPressed(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://maps.apple.com/maps?daddr=\(latitude),\(longitude)")!, options: [:], completionHandler: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func callUsBtnPressed(_ sender: Any) {
     }
-    */
+    
 
 }
