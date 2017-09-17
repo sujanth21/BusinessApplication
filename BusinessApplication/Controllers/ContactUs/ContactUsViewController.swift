95e2ef408acd2bbbff4b2d759cd30dbf9abad61a
Sujanth <sujanth1987@gmail.com>
2017-09-17 11:18:31 +1000

    Map view added

---
+++ BusinessApplication/Controllers/ContactUs/ContactUsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ContactUsViewController.swift
+//  BusinessApplication
+//
+//  Created by Sujanth Sebamalaithasan on 17/9/17.
+//  Copyright © 2017 Sujanth Sebamalaithasan. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ContactUsViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
